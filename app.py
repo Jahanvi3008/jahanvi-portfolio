93d690c964d39f10341f3fb8523c8da99fafcd22
Jahanvi3008 <jahanvi08dave@gmail.com>
2025-07-31 13:00:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -141,9 +141,7 @@ with open("Jahanvi_CV_CoverLetter.pdf", "rb") as file:
         mime="application/pdf"
     )
 
-st.markdown("""
-<iframe src="https://raw.githubusercontent.com/Jahanvi3008/portfolio-assets/main/Jahanvi_CV_CoverLetter.pdf" width="100%" height="800px" style="border: none; border-radius: 10px;"></iframe>
-""", unsafe_allow_html=True)
+
 
 # ---- CONTACT ----
 st.write("---")
